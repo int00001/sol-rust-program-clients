0e0485cd969bbe1236a0be3bfcaa1c0629032075
int <int_eth@proton.me>
2023-04-18 15:47:37 -0500

    feat(client): invoke program

--- hello_world/src/config.ts
+++ hello_world/src/config.ts
@@ -1,1 +1,1 @@
-export const PROGRAM_ID = '';
+export const PROGRAM_ID = 'A77kCESdpvd6LdNhQQTrpYAqPkVg2oX6vjALUJsnJXif';
--- hello_world/src/index.ts
+++ hello_world/src/index.ts
@@ -1,3 +1,30 @@
-const main = async () => { };
+import {
+  Connection,
+  PublicKey,
+  sendAndConfirmTransaction,
+  Transaction,
+  TransactionInstruction,
+} from '@solana/web3.js';
+import * as dotenv from 'dotenv';
+
+import { PROGRAM_ID } from 'config';
+import { loadWallet } from 'utils';
+
+dotenv.config();
+
+const main = async () => {
+  const connection = new Connection(process.env.OFFICIAL_SOL_DEV_HTTPS!);
+  const wallet = loadWallet();
+
+  const programId = new PublicKey(PROGRAM_ID);
+
+  const ix = new TransactionInstruction({
+    keys: [],
+    programId,
+  });
+  const tx = new Transaction().add(ix);
+  const sig = await sendAndConfirmTransaction(connection, tx, [wallet]);
+  console.log(sig);
+};
 
 main();
